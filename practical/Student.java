c2fde6e214859e2c8fb4f332a59fde40000d4194
Shivlilap <105425924+Shivlilap@users.noreply.github.com>
2022-05-24 16:16:44 +0530

    Add files via upload

---
+++ practical/Student.java
@@ -0,0 +1,27 @@
+package Student;
+
+public class Student {
+	
+		int id;
+		String name;
+	    int age;
+		
+		Student(int i,String n){
+		id = i ;
+		name = n;
+		}
+		Student(int i,String n,int a){
+		id = i;
+		name = n;
+		age=a;
+		}
+		void display(){System.out.println(id+" "+name+" "+age);}
+		public static void main(String args[]) {
+		Student s1 = new Student(314,"Madhavi",21);
+		Student s2 = new Student(335,"Shivlila",22);
+		s1.display();
+		s2.display();
+
+	}
+
+}
